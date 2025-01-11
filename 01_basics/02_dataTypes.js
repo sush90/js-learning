241394ae271cec0b4efb06cb5f7bf01c5f222808
Sushma Upadhayay <70304899+sush90@users.noreply.github.com>
2025-01-11 03:34:40 +0000

    datatype

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,4 +2,32 @@
 
 // alert(3+3) // we are using nodejs, not browser
 
-console.log(3+3); console.log("Sushma")
+console.log(3+
+    3)          //code readibility should be high
+console.log("Sushma")
+
+console.log("Sushma")
+
+let name = "sushma"
+let age = 23
+let isLoggedIn = false
+let state;
+
+//number => 2 to power 53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+
+//object
+
+console.log(typeof "Sushma");
+console.log(typeof isLoggedIn);
+console.log(typeof null);   //output will be object
+console.log(typeof undefined);  //output will be undefined
+
+
